b7712fb1c4ab4f24890f3797de4f6c1a5a832267
wzysws@gmail.com <wzysws@gmail.com>
2019-08-05 21:57:34 +0800

    Site updated: 2019-08-05 21:57:28

--- dist/music.js
+++ dist/music.js
@@ -4,9 +4,9 @@ const ap = new APlayer({
     autoplay: true,
     audio: [
       {
-        name: "红玫瑰",
+        name: "孤独患者",
         artist: '陈奕迅',
-        url: 'http://other.web.ri01.sycdn.kuwo.cn/e8764f39f47763eb4750162fc9429cd3/5d412741/resource/n2/47/79/1762261471.mp3',
+        url: 'http://m10.music.126.net/20190805221037/98472ab1a197b81ecff4e84269661676/ymusic/dbc0/6141/a7e2/5d049d85c0784326e0e48726835ec652.mp3',
         cover: 'http://p1.music.126.net/o_OjL_NZNoeog9fIjBXAyw==/18782957139233959.jpg',
       },
       {
@@ -20,6 +20,25 @@ const ap = new APlayer({
         artist: '洛天依',
         url: 'http://www.ytmp3.cn/down/52899.mp3',
         cover: 'http://p2.music.126.net/tO9-64uPOX4d9pl5KEiKvg==/109951163281881755.jpg',
+      },
+      {
+        name: '杀死那个石家庄人',
+        artist: '万能青年旅店',
+        url: 'http://m10.music.126.net/20190805221402/45ed89078f42252e4587d5628ca9009a/ymusic/07fd/0da0/c92b/299e0783a33ed9fdd1d6678760c7fa8a.mp3',
+        cover: 'http://p2.music.126.net/W1kczDCB4-r-uNAznD1ljg==/108851651165850.jpg',
+      },
+      {
+        name: '写给我第一个喜欢的女孩的歌',
+        artist: '洛少爷',
+        url: 'http://m10.music.126.net/20190805221701/4f6bb4faf8c6997c8363c99e2ba313c8/ymusic/59ff/12be/37fd/163b951f1076b1c25f2da97c9d718084.mp3',
+        cover: 'http://p2.music.126.net/UdpoOGOhu2l2Fi7i_LlprA==/18279380812114513.jpg',
+      },
+      {
+        name: '忧伤的巨人',
+        artist: '华晨宇',
+        url: 'http://m10.music.126.net/20190805221848/3fb76ae45f61c5b80011cf382776f1d1/ymusic/19dc/c7fd/62bf/fb7fce1c1bc11fea0a382eb2ebe31148.mp3',
+        cover: 'http://p2.music.126.net/UsSAd3Bdf77DjhCuTSEvUw==/109951163077613693.jpg',
       }
     ]
 });
+
